38a7b58e7e4d155b0ba060e5050a68c1d3d8bbe2
Brian Howell <bigbhowell@gmail.com>
2014-12-06 22:42:58 -0500

    Removed template methods for actionbar.

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/SplashActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/SplashActivity.java
@@ -2,8 +2,7 @@ package com.bitcoin.tracker.walletx;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 public class SplashActivity extends Activity {
 
@@ -13,26 +12,4 @@ public class SplashActivity extends Activity {
         setContentView(R.layout.activity_splash);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_splash, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+} // SplashActivity
