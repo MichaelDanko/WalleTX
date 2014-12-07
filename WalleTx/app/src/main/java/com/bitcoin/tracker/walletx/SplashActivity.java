f1e8d546e259821dc15186734d9dda66240a1f8c
Brian Howell <bigbhowell@gmail.com>
2014-12-06 22:56:26 -0500

    Added TODO comments to splash activity.

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/SplashActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/SplashActivity.java
@@ -3,13 +3,25 @@ package com.bitcoin.tracker.walletx;
 import android.app.Activity;
 import android.os.Bundle;
 
-
+/**
+ * SplashActivity is the entry portal for Bitcoin WalleTx.
+ *
+ * Activity should start a background service to fetch new transactions & price data
+ * before redirecting to the MainActivity. Display for a minimum of 2 seconds.
+ *
+ */
 public class SplashActivity extends Activity {
+
+    final private long delay = 2000; //minimum amount of time to display
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // TODO - Start service to fetch new tx and price data
+        // TODO - Add TaskTimer that redirects to main
+
     }
 
 } // SplashActivity
