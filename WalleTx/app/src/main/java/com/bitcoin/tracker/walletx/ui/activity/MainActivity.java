717c40b578ef9630db42b4be433c1d3cab1d5962
Brian Howell <bigbhowell@gmail.com>
2014-12-13 00:24:29 -0500

    Added fragments for each item in the navigation drawer. Code is a mess right atm. Will clean up with some comments now.

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/ui/activity/MainActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/ui/activity/MainActivity.java
@@ -1,30 +1,32 @@
 package com.bitcoin.tracker.walletx.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 import com.bitcoin.tracker.walletx.R;
+import com.bitcoin.tracker.walletx.ui.fragment.ManageTxCategoriesFragment;
+import com.bitcoin.tracker.walletx.ui.fragment.ManageWalletGroupsFragment;
+import com.bitcoin.tracker.walletx.ui.fragment.MyWalletsFragment;
 import com.bitcoin.tracker.walletx.ui.fragment.NavigationDrawerFragment;
+import com.bitcoin.tracker.walletx.ui.fragment.SettingsFragment;
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
-    // Fragment managing the behaviors, interactions and presentation
-    // of the navigation drawer.
+    /**
+     * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+     */
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
-    // Used to store the last screen title.
-    // For use in {@link #restoreActionBar()}.
+    /**
+     * Used to store the last screen title. For use in {@link #restoreActionBar()}.
+     */
     private CharSequence mTitle;
 
     @Override
@@ -42,12 +44,35 @@ public class MainActivity extends ActionBarActivity
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
 
+    /**
+     * Updates the main content by replacing fragments
+     * @param position
+     */
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
+
+        Fragment fragment = null;
+        switch (position) {
+            case 0:
+                fragment = MyWalletsFragment.newInstance(position + 1);
+                break;
+            case 1:
+                fragment = ManageWalletGroupsFragment.newInstance(position + 1);
+                break;
+            case 2:
+                fragment = ManageTxCategoriesFragment.newInstance(position + 1);
+                break;
+            case 3:
+                fragment = SettingsFragment.newInstance(position + 1);
+                break;
+            default:
+                fragment = MyWalletsFragment.newInstance(position + 1);
+                break;
+        }
+
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
+                .replace(R.id.container, fragment)
                 .commit();
     }
 
@@ -104,45 +129,5 @@ public class MainActivity extends ActionBarActivity
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-
-        @Override
-        public void onAttach(Activity activity) {
-            super.onAttach(activity);
-            ((MainActivity) activity).onSectionAttached(
-                    getArguments().getInt(ARG_SECTION_NUMBER));
-        }
-    }
-
-}
+} // MainActivity
 
