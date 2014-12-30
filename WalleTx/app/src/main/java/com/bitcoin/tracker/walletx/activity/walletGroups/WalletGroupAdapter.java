017e7600d094d5f8c9d5b767f50ab775038226c2
Brian W. Howell <bigbhowell@gmail.com>
2014-12-30 17:17:19 -0500

    WalletGroups: Working on allowing the user to change the display order. I was originally going to implement drag and drop to accomplish this but it is proving rather difficult. In an effort to save time I'm implementing a simpler, less elegant method using buttons to allow for reordering. Current implementation and UI is incomplete.

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/walletGroups/WalletGroupAdapter.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/walletGroups/WalletGroupAdapter.java
@@ -4,12 +4,18 @@ import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 
+import com.activeandroid.query.Select;
 import com.bitcoin.tracker.walletx.R;
+import com.bitcoin.tracker.walletx.model.wallet.WalletGroup;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Populates custom list items in the WalletGroup list view.
@@ -26,7 +32,7 @@ public class WalletGroupAdapter extends ArrayAdapter<WalletGroupListItem> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
 
         // Create inflater
         LayoutInflater inflater = (LayoutInflater) activity
@@ -36,12 +42,94 @@ public class WalletGroupAdapter extends ArrayAdapter<WalletGroupListItem> {
         View rowView = inflater.inflate(R.layout.fragment_walletgroup_list_item, parent, false);
         TextView groupName = (TextView) rowView.findViewById(R.id.groupNameLabel);
         TextView defaultGroup = (TextView) rowView.findViewById(R.id.defaultGroupLabel);
+        ImageButton moveDown = (ImageButton) rowView.findViewById(R.id.imageButtonMoveDown);
+        ImageButton moveUp = (ImageButton) rowView.findViewById(R.id.imageButtonMoveUp);
+
+        // Stop buttons from preventing list item click event
+        // http://stackoverflow.com/questions/11160639/list-item-with-button-not-clickable-anymore
+        moveDown.setFocusable(false);
+        moveDown.setFocusableInTouchMode(false);
+        moveUp.setFocusable(false);
+        moveUp.setFocusableInTouchMode(false);
+
+        // Disable the up and down buttons for the first and last list items.
+        if (position == 0) {
+            moveUp.setEnabled(false);
+            moveUp.setVisibility(View.INVISIBLE);
+        }
+        if (position + 1 == WalletGroup.getLast().displayOrder) {
+            moveDown.setEnabled(false);
+            moveDown.setVisibility(View.INVISIBLE);
+        }
 
         // Set the text for textView
         groupName.setText(itemsArrayList.get(position).getName());
         defaultGroup.setText(itemsArrayList.get(position).getIsDefault());
 
+        // On click move the wallet group up or down and change its displayOrder.
+        moveDown.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // TODO ADD THIS FUNCTIONALITY
+                System.out.println("MOVE DOWN");
+            }
+
+        });
+
+        moveUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                int displayOrder = position + 1;
+                WalletGroup clicked = new Select()
+                        .from(WalletGroup.class)
+                        .where("DisplayOrder = ?", position + 1)
+                        .executeSingle();
+                WalletGroup swap = new Select()
+                        .from(WalletGroup.class)
+                        .where("DisplayOrder = ?", position)
+                        .executeSingle();
+                swap.displayOrder = position + 1;
+                clicked.displayOrder = position;
+                swap.save();
+                clicked.save();
+
+                refreshListView(parent);
+
+
+                WalletGroup.dump();
+
+            }
+        });
+
         // Return rowView
         return rowView;
     }
+
+    /**
+     * Refreshes the content of the parent list view
+     * @param parent
+     */
+    private void refreshListView(ViewGroup parent) {
+        ListAdapter adapter;
+        ArrayList<WalletGroupListItem> items = new ArrayList<WalletGroupListItem>();
+        List<WalletGroup> groups = WalletGroup.getAll();
+        for (WalletGroup group : groups) {
+            WalletGroupListItem item;
+            // Hide form components.
+            if (group.isDefault()) {
+                item = new WalletGroupListItem(group.name,
+                        activity.getString(R.string.label_default_wallet_group));
+            } else {
+                item = new WalletGroupListItem(group.name, "");
+            }
+            items.add(item);
+        }
+        adapter = new WalletGroupAdapter(activity, items);
+        ((AdapterView<ListAdapter>) parent).setAdapter(adapter);
+    }
+
 }
