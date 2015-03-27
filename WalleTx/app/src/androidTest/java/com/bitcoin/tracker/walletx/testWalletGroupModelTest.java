8d890d7f20ea44b8ea58a078c2a2d52bfc9f8b7e
Daniel Carroll <danielc103@gmail.com>
2015-03-26 22:46:05 -0400

    homework testing

--- WalleTx/app/src/androidTest/java/com/bitcoin/tracker/walletx/testWalletGroupModelTest.java
+++ WalleTx/app/src/androidTest/java/com/bitcoin/tracker/walletx/testWalletGroupModelTest.java
@@ -1,25 +1,21 @@
-package com.bitcoin.tracker.walletx.UnitTest;
+package com.bitcoin.tracker.walletx;
 
 import android.test.suitebuilder.annotation.SmallTest;
 
-import com.bitcoin.tracker.walletx.model.Balance;
 import com.bitcoin.tracker.walletx.model.QueryModelTest;
 import com.bitcoin.tracker.walletx.model.WalletGroup;
-import com.bitcoin.tracker.walletx.model.Walletx;
-
-import junit.framework.TestCase;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by DanielCarroll on 3/22/2015.
  */
-public class ModelTest extends TestCase {
+
+/*
+This test will test building part of the database (specifically the WalletGroup) then test the
+results of the queries in the WalletGroup Model
+ */
+public class testWalletGroupModelTest extends ApplicationTest {
 
     //will throw exception if setup incorrectly
     @Override
@@ -30,7 +26,7 @@ public class ModelTest extends TestCase {
 
     //run small test against WalletGroup Query model
     @SmallTest
-    public void balanceQueryTest() throws ParseException {
+    public void testBalanceQueryTest() throws ParseException {
 
 
         QueryModelTest newTest = new QueryModelTest();
@@ -51,7 +47,7 @@ public class ModelTest extends TestCase {
 
     }
 
-    //will throw exception if tear down is incorrect 
+    //will throw exception if tear down is incorrect
     @Override
     protected void tearDown() throws Exception{
         super.tearDown();
