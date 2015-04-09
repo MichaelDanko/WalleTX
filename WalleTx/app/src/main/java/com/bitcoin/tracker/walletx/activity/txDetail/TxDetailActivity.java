0b055d969f3087fad574e66dce8940e3cc6e7434
Brian Howell <bigbhowell@gmail.com>
2015-04-09 11:39:02 -0500

    TxDetail action bar setup

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txDetail/TxDetailActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txDetail/TxDetailActivity.java
@@ -13,28 +13,31 @@ public class TxDetailActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tx_detail);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
+    //region OPTIONS MENU
+
+    /**
+     * Display the global options menu.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tx_detail, menu);
+        getMenuInflater().inflate(R.menu.global, menu);
         return true;
     }
 
+    /**
+     * Home button closes the activity.
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+        if (id == android.R.id.home)
+            finish();
         return super.onOptionsItemSelected(item);
     }
+
+    //endregion
 }
