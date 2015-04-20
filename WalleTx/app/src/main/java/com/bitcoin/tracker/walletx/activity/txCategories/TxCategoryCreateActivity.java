0a8568099e367851b4993536559ff24dc046db95
asolano7 <arronsolano@yahoo.com>
2015-04-19 21:00:01 -0400

    Updated the TxCategory stuff

--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txCategories/TxCategoryCreateActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txCategories/TxCategoryCreateActivity.java
@@ -1,11 +1,17 @@
 package com.bitcoin.tracker.walletx.activity.txCategories;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import com.bitcoin.tracker.walletx.R;
+import com.bitcoin.tracker.walletx.model.TxCategory;
 
 /*
 
@@ -16,16 +22,43 @@ import com.bitcoin.tracker.walletx.R;
  */
 public class TxCategoryCreateActivity extends ActionBarActivity {
 
+    private EditText mCatName;
+    private Button mSubmit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_tx_category);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupActionBar();
+        getViewsById();
+        addSubmitButtonClickListener();
     }
 
+    private void setupActionBar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Add New Category");
+    }
+
+    private void getViewsById(){
+        mCatName = (EditText) findViewById(R.id.category_create_edit_text);
+        mSubmit = (Button) findViewById(R.id.category_create_button);
+    }
 
     //region OPTIONS MENU
+    private void addSubmitButtonClickListener(){
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameEntered = mCatName.getText().toString().toLowerCase();
+                String name = mCatName.getText().toString();
+                TxCategory.createTxCategory(name, false);
 
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
     /**
      * Closes activity when the home button is selected.
      */
--- WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txCategories/TxCategoryUpdateActivity.java
+++ WalleTx/app/src/main/java/com/bitcoin/tracker/walletx/activity/txCategories/TxCategoryUpdateActivity.java
@@ -1,11 +1,19 @@
 package com.bitcoin.tracker.walletx.activity.txCategories;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.bitcoin.tracker.walletx.R;
+import com.bitcoin.tracker.walletx.model.TxCategory;
 
 /*
 
@@ -17,15 +25,93 @@ import com.bitcoin.tracker.walletx.R;
 
  */
 public class TxCategoryUpdateActivity extends ActionBarActivity {
+    private EditText mCatName;
+    private String   mCurrentName;
+    private Button   mUpdate;
+    private Button   mDelete;
+    private TextView mCannotDeleteLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tx_category_update);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupActionBar();
+        getViewsById();
+        bindClickEvents();
+        addCurrentCategoryNameToEditText();
+        //disableElementsForDefaultCat();
     }
 
+    private void setupActionBar(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Category_Update_Activity");
+    }
 
+    private void getViewsById(){
+        mCatName = (EditText) findViewById(R.id.category_update_edit_text);
+        mUpdate = (Button) findViewById(R.id.category_update_button);
+        mDelete = (Button) findViewById(R.id.category_delete_button);
+    }
+
+    private void bindClickEvents(){
+        mUpdate.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                TxCategory catBeingUpdated = TxCategory.getBy(mCurrentName);
+                String nameInEditText = mCatName.getText().toString().toLowerCase();
+                String nameOfCatBeingUpdated = mCurrentName.toLowerCase();
+                boolean nameNotChanged = nameInEditText.equals(nameOfCatBeingUpdated);
+
+                if(nameNotChanged){
+                    finish();
+                }else{
+                    TxCategory update = TxCategory.getBy(mCurrentName);
+                    update.update(mCatName.getText().toString(), true);
+                }
+
+                finishWithResultOk();
+            }
+        });
+
+        mDelete.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                builder.setMessage("Update alert message Delete");
+                builder.setTitle("Delete category'" + mCurrentName + "'?");
+                builder.setPositiveButton(R.string.app_confirm_yes, new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.dismiss();
+                        TxCategory toDelete = TxCategory.getBy(mCurrentName);
+                        TxCategory.deleteCategory(toDelete);
+                        finishWithResultOk();
+                    }
+                });
+                builder.setNegativeButton(R.string.app_confirm_no, new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.dismiss();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }//end bindClickEvents
+
+    private void addCurrentCategoryNameToEditText(){
+        Intent intent = getIntent();
+        mCurrentName = intent.getStringExtra("wallet_category_name");
+        mCatName.setText(mCurrentName);
+    }
+
+    private void finishWithResultOk(){
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
+    }
     //region OPTIONS MENU
 
     /**
